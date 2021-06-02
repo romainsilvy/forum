953db2316f761288171aa450d205a442b4f00481
amaury <amaury.lyonnet13@gmail.com>
2021-06-02 10:16:22 +0200

    avancer page mon profil

--- server.go
+++ server.go
@@ -23,6 +23,7 @@ func cssFile() {
 	http.Handle("/global.css", cssServer)
 	http.Handle("/accueil.css", cssServer)
 	http.Handle("/accueil-droit.css", cssServer)
+	http.Handle("/monprofil.css", cssServer)
 }
 
 func pictureFile() {
@@ -58,7 +59,26 @@ func getUsers() {
 	})
 }
 
+func monProfil() {
+	http.HandleFunc("/monprofil", func(w http.ResponseWriter, r *http.Request) {
+		variable, _ := template.ParseFiles("monprofil.html")
+		database, _ := sql.Open("sqlite3", "./forum.db")
+		rows, _ := database.Query("select * from User")
+		var result []User
+		for rows.Next() {
+			item := User{}
+			err2 := rows.Scan(&item.Id_user, &item.User_name, &item.Password, &item.Email)
+			if err2 != nil {
+				panic(err2)
+			}
+			result = append(result, item)
+		}
+		variable.Execute(w, result)
+	})
+}
+
 func main() {
+	monProfil()
 	getUsers()
 	cssFile()
 	pictureFile()
