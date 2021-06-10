e85942fcde70ae86757b67aad79fbaee7f638679
RSILVYLELIGOIS <romain.silvyleligois@ynov.com>
2021-06-10 17:10:21 +0200

    avancement lien front et bdd

--- server.go
+++ server.go
@@ -1,6 +1,5 @@
 package main
 
-// idée a faire : mettre la base de donnée dans sql web et faire la commande ligne 69 ^^ INSHALLAH CA MARCHE
 import (
 	"database/sql"
 	"fmt"
@@ -8,16 +7,12 @@ import (
 	"log"
 	"net/http"
 	databaseTools "test/dataBase"
-
-	// "forum/dataBase/database"
-
 	"github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
-	key   = []byte("super-secret-key")
+	key   = []byte("ismatheplatypus@w*")
 	store = sessions.NewCookieStore(key)
 )
 
@@ -29,75 +24,77 @@ func runServer() {
 	}
 }
 
+//inscription manage the inscription popup 
+func inscription(r *http.Request)  {
+	if r.FormValue("inscriptionPseudo") != "" {
+		inscriptionPseudo := r.FormValue("inscriptionPseudo")
+		inscriptionEmail := r.FormValue("inscriptionEmail")
+		inscriptionEmailConfirm := r.FormValue("inscriptionEmailConfirm")
+		inscriptionPassword := r.FormValue("inscriptionPassword")
+		inscriptionPasswordConfirm := r.FormValue("inscriptionPasswordConfirm")
+
+		if inscriptionEmail == inscriptionEmailConfirm && inscriptionPassword == inscriptionPasswordConfirm {
+			databaseTools.InsertIntoUsers(inscriptionPseudo, inscriptionEmail, inscriptionPassword, "img")
+		}
+	}
+}
+
+//connexion manage the connexion popup 
+func connexion(w http.ResponseWriter, r *http.Request, database *sql.DB)  {
+	if r.FormValue("connect") != "" {
+		connexionUser := r.FormValue("connexionUser")
+		connexionPassword := r.FormValue("connexionPassword")
+		checkIfExist := checkIfExist(database, "password", "User", "user_name", connexionUser)
+
+		if checkIfExist {
+			userPassword := singleRowQuerry(database, "password", "User", "user_name", connexionUser)
+			if userPassword != "notExist" {
+				if userPassword == connexionPassword {
+					session, _ := store.Get(r, "auth")
+					session.Values["authenticated"] = true
+					session.Values["user"] = connexionUser
+					session.Save(r, w)
+					fmt.Println("tu es co chacal")
+				} else {
+					fmt.Println("mauvais mdp chacal")
+				}
+			}
+		} else {
+			fmt.Println("mauvais pseudo chacal")
+		}
+	}
+}
+
+
+//handleAccueil is the handlefunc for the main page 
 func handleAccueil(oneUser databaseTools.User, tabUser []databaseTools.User, database *sql.DB) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		variable, _ := template.ParseFiles("index.html")
-
-		if r.FormValue("inscriptionPseudo") != "" {
-			inscriptionPseudo := r.FormValue("inscriptionPseudo")
-			inscriptionEmail := r.FormValue("inscriptionEmail")
-			inscriptionEmailConfirm := r.FormValue("inscriptionEmailConfirm")
-			inscriptionPassword := r.FormValue("inscriptionPassword")
-			inscriptionPasswordConfirm := r.FormValue("inscriptionPasswordConfirm")
-
-			if inscriptionEmail == inscriptionEmailConfirm && inscriptionPassword == inscriptionPasswordConfirm {
-				databaseTools.InsertIntoUsers(inscriptionPseudo, inscriptionEmail, inscriptionPassword, "test")
-			}
-		}
-
-		if r.FormValue("connect") != "" {
-			connexionUser := r.FormValue("connexionUser")
-			connexionPassword := r.FormValue("connexionPassword")
-			checkIfExist := checkIfExist(database, "password", "User", "user_name", connexionUser)
-
-			if checkIfExist {
-				userPassword := singleRowQuerry(database, "password", "User", "user_name", connexionUser)
-				if userPassword != "notExist" {
-					if userPassword == connexionPassword {
-						session, _ := store.Get(r, "auth")
-
-						// Authentication goes here
-						// ...
-
-						// Set user as authenticated
-						session.Values["authenticated"] = true
-						session.Save(r, w)
-						fmt.Println("tu es co chacal")
-					} else {
-						fmt.Println("mauvais mdp chacal")
-					}
-				}
-			} else {
-				fmt.Println("mauvais pseudo chacal")
-			}
-		}
-
+		inscription(r)
+		connexion(w, r, database)
 		variable.Execute(w, tabUser)
 	})
 }
 
+//handleProfil is the handlefunc for the profil page 
 func handleProfil(oneUser databaseTools.User, tabUser []databaseTools.User, database *sql.DB) {
 	http.HandleFunc("/profil/", func(w http.ResponseWriter, r *http.Request) {
 		variable, _ := template.ParseFiles("profil.html")
-		rows, _ := database.Query("select * from User")
-		result := tabUser
-		for rows.Next() {
-			item := oneUser
-			err2 := rows.Scan(&item.Id_user, &item.User_name, &item.Password, &item.Email, &item.Image)
-			if err2 != nil {
-				panic(err2)
-			}
-			result = append(result, item)
-		}
+		
 		session, _ := store.Get(r, "auth")
+		username := session.Values["user"].(string)
+		oneUser.User_name = username
+		oneUser.Email = singleRowQuerry(database, "email", "User", "user_name", oneUser.User_name)
+		
 		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
 			http.Error(w, "Forbidden", http.StatusForbidden)
 			return
 		}
-		variable.Execute(w, result)
+		variable.Execute(w, oneUser)
 	})
 }
 
+//singleRowQuerry retrieve a value in the db with a where comparator
 func singleRowQuerry(db *sql.DB, rowName string, tableName string, comparator1 string, comparator2 string) string {
 	stmt, err := db.Prepare("select " + rowName + " from " + tableName + " where " + comparator1 + " = ?")
 	if err != nil {
@@ -112,6 +109,8 @@ func singleRowQuerry(db *sql.DB, rowName string, tableName string, comparator1 s
 	return toReturn
 }
 
+
+//checkIfExist return true or false depending if the comparator 1 passed as parameter exist in the db 
 func checkIfExist(db *sql.DB, rowName string, tableName string, comparator1 string, comparator2 string) bool {
 	stmt, err := db.Prepare("select " + rowName + " from " + tableName + " where " + comparator1 + " = ?")
 	if err != nil {
