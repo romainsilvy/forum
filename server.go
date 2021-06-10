b6d594703f158ad674db4a98619ad3ab5493f30e
ayato <louisreynard919@gmail.com>
2021-06-10 12:09:51 +0200

    connection / incriscption and cookie worked

--- server.go
+++ server.go
@@ -12,15 +12,15 @@ import (
 	// "forum/dataBase/database"
 
 	"github.com/gorilla/sessions"
+	_ "github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// type User struct {
-// 	Id_user   int
-// 	User_name string
-// 	Password  string
-// 	Email     string
-// }
+var (
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
 
 //runServer sets the listenandserve port to 8080
 func runServer() {
@@ -33,11 +33,6 @@ func runServer() {
 func handleAccueil(oneUser databaseTools.User, tabUser []databaseTools.User, database *sql.DB) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		variable, _ := template.ParseFiles("index.html")
-		http.SetCookie(w, &http.Cookie{
-			Name:  "my-cookie",
-			Value: "some value",
-			Path:  "/",
-		})
 
 		if r.FormValue("inscriptionPseudo") != "" {
 			inscriptionPseudo := r.FormValue("inscriptionPseudo")
@@ -60,7 +55,15 @@ func handleAccueil(oneUser databaseTools.User, tabUser []databaseTools.User, dat
 				userPassword := singleRowQuerry(database, "password", "User", "user_name", connexionUser)
 				if userPassword != "notExist" {
 					if userPassword == connexionPassword {
-						fmt.Println("tu est co chacal")
+						session, _ := store.Get(r, "auth")
+
+						// Authentication goes here
+						// ...
+
+						// Set user as authenticated
+						session.Values["authenticated"] = true
+						session.Save(r, w)
+						fmt.Println("tu es co chacal")
 					} else {
 						fmt.Println("mauvais mdp chacal")
 					}
@@ -73,86 +76,6 @@ func handleAccueil(oneUser databaseTools.User, tabUser []databaseTools.User, dat
 		variable.Execute(w, tabUser)
 	})
 }
-
-var (
-	key   = []byte("super-secret-key")
-	store = sessions.NewCookieStore(key)
-)
-
-func secret(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
-
-	// Check if user is authenticated
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
-	}
-
-	// Print secret message
-	fmt.Fprintln(w, "The cake is a lie!")
-}
-
-// get Data from the sqlite database and print them int the html page
-// func getIncrisption(oneUser databaseTools.User, tabUser []databaseTools.User, database *sql.DB) {
-// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 		variable, _ := template.ParseFiles("index.html")
-// 		http.SetCookie(w, &http.Cookie{
-// 			Name:  "my-cookie",
-// 			Value: "some value",
-// 			Path:  "/",
-// 		})
-// 		fmt.Println("COOKIE CREATED")
-
-// 		if r.FormValue("inscriptionPseudo") != "" {
-// 			inscriptionPseudo := r.FormValue("inscriptionPseudo")
-// 			inscriptionEmail := r.FormValue("inscriptionEmail")
-// 			inscriptionEmailConfirm := r.FormValue("inscriptionEmailConfirm")
-// 			inscriptionPassword := r.FormValue("inscriptionPassword")
-// 			inscriptionPasswordConfirm := r.FormValue("inscriptionPasswordConfirm")
-
-// 			if inscriptionEmail == inscriptionEmailConfirm && inscriptionPassword == inscriptionPasswordConfirm {
-// 				databaseTools.InsertIntoUsers(inscriptionPseudo, inscriptionEmail, inscriptionPassword, "test")
-// 			}
-// 		}
-// 		rows, _ := database.Query("select * from User")
-// 		result := tabUser
-// 		for rows.Next() {
-// 			item := oneUser
-// 			err2 := rows.Scan(&item.Id_user, &item.User_name, &item.Password, &item.Email, &item.Image)
-// 			if err2 != nil {
-// 				panic(err2)
-// 			}
-// 			result = append(result, item)
-// 		}
-// 		variable.Execute(w, result)
-// 	})
-// }
-
-// func getConnexion(oneUser databaseTools.User, tabUser []databaseTools.User, database *sql.DB) {
-// 	http.HandleFunc("/connexion", func(w http.ResponseWriter, r *http.Request) {
-// 		variable, _ := template.ParseFiles("index.html")
-
-// 		connexionUser := r.FormValue("connexionUser")
-// 		// connexionPassword := r.FormValue("connexionPassword")
-
-// 		rows, _ := database.Query("select * from User")
-// 		result := tabUser
-// 		for rows.Next() {
-// 			item := oneUser
-// 			err2 := rows.Scan(&item.User_name)
-// 			if err2 != nil {
-// 				panic(err2)
-// 			}
-// 			if connexionUser == item.User_name {
-// 				fmt.Println("c est good")
-// 			} else {
-// 				fmt.Println("marche pas")
-// 			}
-// 			result = append(result, item)
-// 		}
-// 		variable.Execute(w, result)
-// 	})
-// }
 
 func handleProfil(oneUser databaseTools.User, tabUser []databaseTools.User, database *sql.DB) {
 	http.HandleFunc("/profil/", func(w http.ResponseWriter, r *http.Request) {
@@ -167,18 +90,13 @@ func handleProfil(oneUser databaseTools.User, tabUser []databaseTools.User, data
 			}
 			result = append(result, item)
 		}
+		session, _ := store.Get(r, "auth")
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
 		variable.Execute(w, result)
 	})
-}
-
-func set(w http.ResponseWriter, req *http.Request) {
-	http.SetCookie(w, &http.Cookie{
-		Name:  "my-cookie",
-		Value: "some value",
-		Path:  "/",
-	})
-	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
-	fmt.Fprintln(w, "in chrome go to: dev tools / application / cookies")
 }
 
 func singleRowQuerry(db *sql.DB, rowName string, tableName string, comparator1 string, comparator2 string) string {
@@ -213,57 +131,8 @@ func main() {
 	databaseOpened, _ := sql.Open("sqlite3", "dataBase/forum.db")
 	fileServer := http.FileServer(http.Dir("./data"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
-
 	handleAccueil(databaseTools.User{}, []databaseTools.User{}, databaseOpened)
-	// handleProfil(databaseTools.User{}, []databaseTools.User{}, databaseOpened)
+	handleProfil(databaseTools.User{}, []databaseTools.User{}, databaseOpened)
+
 	runServer()
 }
-
-// func secret(w http.ResponseWriter, r *http.Request) {
-//     session, _ := store.Get(r, "cookie-name")
-
-//     // Check if user is authenticated
-//     if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-//         http.Error(w, "Forbidden", http.StatusForbidden)
-//         return
-//     }
-
-//     // Print secret message
-//     fmt.Fprintln(w, "The cake is a lie!")
-// }
-
-// func login(w http.ResponseWriter, r *http.Request) {
-//     session, _ := store.Get(r, "cookie-name")
-
-// 	//CHECK PASSWORD AND EMAIL
-//     session.Values["authenticated"] = true
-//     session.Save(r, w)
-// }
-
-// func logout(w http.ResponseWriter, r *http.Request) {
-//     session, _ := store.Get(r, "cookie-name")
-
-//     // Revoke users authentication
-//     session.Values["authenticated"] = false
-//     session.Save(r, w)
-// }
-
-// func mainConnexion() {
-//     http.HandleFunc("/secret", secret)
-//     http.HandleFunc("/login", login)
-//     http.HandleFunc("/logout", logout)
-
-//     http.ListenAndServe(":8080", nil)
-// }
-
-//LIRE BDD ET ENVOYER
-
-// for rows.Next() {
-// 	item := oneUser
-// 	err2 := rows.Scan(&item.Id_user, &item.User_name, &item.Password, &item.Email, &item.Image)
-// 	if err2 != nil {
-// 		panic(err2)
-// 	}
-// 	result = append(result, item)
-// }
-// variable.Execute(w, result)
--- trash/session.go
+++ trash/session.go
@@ -4,51 +4,107 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/sessions"
+	"github.com/gorilla/securecookie"
 )
 
-var (
-	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
-	key   = []byte("super-secret-key")
-	store = sessions.NewCookieStore(key)
-)
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
 
-func secret(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
-
-	// Check if user is authenticated
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
+func getSession(request *http.Request) (yourName string) {
+	if cookie, err := request.Cookie("your-name"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("your-name", cookie.Value, &cookieValue); err == nil {
+			yourName = cookieValue["your-name"]
+		}
 	}
-
-	// Print secret message
-	fmt.Fprintln(w, "The cake is a lie!")
+	return yourName
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
-
-	// Authentication goes here
-	// ...
-
-	// Set user as authenticated
-	session.Values["authenticated"] = true
-	session.Save(r, w)
+func setSession(yourName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"your-name": yourName,
+	}
+	if encoded, err := cookieHandler.Encode("your-name", value); err == nil {
+		cookie := &http.Cookie{
+			Name:   "your-name",
+			Value:  encoded,
+			Path:   "/",
+			MaxAge: 3600,
+		}
+		http.SetCookie(response, cookie)
+	}
 }
 
-func logout(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
-
-	// Revoke users authentication
-	session.Values["authenticated"] = false
-	session.Save(r, w)
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "your-name",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
 }
+
+func setSessionHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("name")
+	redirectTarget := "/"
+	if name != "" {
+		setSession(name, response)
+		redirectTarget = "/page1"
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
+
+func clearSessionHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
+}
+
+const indexPage = `
+Sesssion Test
+
+    Your Name
+    
+    Set Sesssion
+
+`
+
+func indexPageHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, indexPage)
+}
+
+const frontPage = `
+Check Sesssion
+
+Your Name: %s
+
+    Clear Sesssion
+
+`
+
+func sessionPageHandler(response http.ResponseWriter, request *http.Request) {
+	yourName := getSession(request)
+	if yourName != "" {
+		fmt.Fprintf(response, frontPage, yourName)
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
+}
+
+var router = mux.NewRouter()
 
 func main() {
-	http.HandleFunc("/secret", secret)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/logout", logout)
 
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/page1", sessionPageHandler)
+	router.HandleFunc("/page2", sessionPageHandler)
+	router.HandleFunc("/page3", sessionPageHandler)
+	router.HandleFunc("/page4", sessionPageHandler)
+
+	router.HandleFunc("/start", setSessionHandler).Methods("POST")
+	router.HandleFunc("/clear", clearSessionHandler).Methods("POST")
+
+	http.Handle("/", router)
 	http.ListenAndServe(":8080", nil)
 }
